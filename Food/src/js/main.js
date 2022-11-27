4865119f343c468be7b66efb6f77dcac0af313e0
Isken <kggisken4ik@gmail.com>
2022-11-27 20:35:45 +0600

    добавлен таймер

--- Food/src/js/main.js
+++ Food/src/js/main.js
@@ -1,11 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
-
+    //Tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
 
-
-    
     function hideTabContent() {
         tabsContent.forEach(item => {
             item.style.display = 'none';
@@ -36,4 +34,60 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+
+    //Timer
+
+    const deadline = '2023-05-11';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t/ (1000 * 60 * 60 *24)),
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+        
+        return {
+            'total': t,
+            'days': days,
+            'housr': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    };
+
+    function getZero(num){
+        if (num >= 0 && num < 10){
+            return `0${num}`
+        } else {
+            return num
+        }
+    };
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              housr = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timerInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            housr.innerHTML = getZero(t.housr);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+
+            if(t.total <= 0) {
+                clearInterval(timerInterval);
+            }
+        }
+    }
+
+
+    setClock('.timer', deadline);
 });
